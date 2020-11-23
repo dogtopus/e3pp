8e5c42615e326ac5de1ba17ffdfaf746030b2776
dogtopus <dogtopus@users.noreply.github.com>
2020-11-23 04:16:57 -0400

    Add untested native PAssingLink DS4Key export

--- e3.py
+++ e3.py
@@ -13,6 +13,8 @@
 import sys
 import argparse
 import functools
+import itertools
+import io
 import traceback
 
 from Crypto.Hash import SHA256
@@ -21,6 +23,40 @@ from Crypto.Signature import PKCS1_PSS
 from Crypto.Math.Numbers import Integer
 from Crypto.Util.number import bytes_to_long
 
+def grouper_no_padding(gen, chunk_size):
+    while len(line := tuple(itertools.islice(gen, chunk_size))) != 0:
+        yield line
+
+def int2mbedmpi(number):
+    def _iter(number):
+        line_break_counter = 0
+        while number != 0:
+            yield f'0x{int(number & 0xffffffff):08x}'
+            line_break_counter += 1
+            number >>= 32
+    return tuple(_iter(number))
+
+def emit_c_array(elements, name, type_, static=False, const=True, chunk_size=4):
+    nmemb = len(elements)
+    result = io.StringIO()
+    if static:
+        result.write('static ')
+    if const:
+        result.write('const ')
+    result.write(type_)
+    result.write(' ')
+    result.write(name)
+    result.write(f'[{nmemb}] = {{\n')
+    for row in grouper_no_padding(iter(elements), chunk_size):
+        result.write('  ')
+        result.write(', '.join(row))
+        result.write(',\n')
+    result.write('};\n')
+    return result.getvalue()
+
+def to_hex_str_tuple(ba):
+    return tuple(f'0x{v:02x}' for v in ba)
+
 def locate_key(fw, e_padding_size=0xfc):
     # RSA public exponent is a very good signature for locating key blocks.
     PATTERN = b'\x00' * e_padding_size + b'\x00\x01\x00\x01'
@@ -28,7 +64,7 @@ def locate_key(fw, e_padding_size=0xfc):
     offset -= 0x110
     return offset
 
-def parse_and_extract_key(fw, offset, e_padding_size=0xfc, prefix=None):
+def parse_and_extract_key(fw, offset, e_padding_size=0xfc, prefix=None, passinglink=False):
     print(f'Serial: {fw[offset:offset+0x10].hex()}')
     offset_sig = offset + 0x110 + e_padding_size + 4
     offset_private = offset + 0x210 + e_padding_size + 4
@@ -69,6 +105,48 @@ def parse_and_extract_key(fw, offset, e_padding_size=0xfc, prefix=None):
             f.write(fw[offset:offset+0x10])
         with open(f'{prefix}-keypair.der', 'wb') as f:
             f.write(keypair.exportKey('DER'))
+        if passinglink:
+            print('Generating PassingLink DS4Key file')
+            mbedmpis = {
+                'n': int2mbedmpi(n),
+                'e': int2mbedmpi(e),
+                'd': int2mbedmpi(d),
+                'p': int2mbedmpi(p),
+                'q': int2mbedmpi(q),
+                'dp': int2mbedmpi(dp1),
+                'dq': int2mbedmpi(dq1),
+                'qp': int2mbedmpi(pq),
+            }
+            ds4id_str = {
+                'key_n': to_hex_str_tuple(n.to_bytes(0x100, 'big')),
+                'key_e': to_hex_str_tuple(e.to_bytes(0x100, 'big')),
+                'serial': to_hex_str_tuple(fw[offset:offset+0x10]),
+                'signature': to_hex_str_tuple(fw[offset_sig:offset_private]),
+            }
+            with open(f'{prefix}.h', 'w') as f:
+                f.write('/* Key factors as mbedTLS MPIs */\n')
+                for factor, mpi in mbedmpis.items():
+                    f.write(emit_c_array(mbedmpis[factor], f'_pl_ds4key_{factor}', 'mbedtls_mpi_uint', static=True))
+                    f.write('\n')
+                # TODO might be good to have RN here so the calculation can be sped up.
+
+                f.write('/* Saved DS4Key mbedTLS RSA context for use without runtime key importing (saves quite a lot of RAM) */\n')
+                f.write('const struct mbedtls_rsa_context __ds4_key = {\n')
+                f.write('  .ver = 0,\n  .len = 256,\n')
+                f.write('  .padding = MBEDTLS_RSA_PKCS_V21, .hash_id = MBEDTLS_MD_SHA256,\n')
+                for factor, mpi in mbedmpis.items():
+                    f.write(f'  .{factor.upper()} = {{ .s=1, .n={len(mpi)}, .p=const_cast<mbedtls_mpi_uint*>(_pl_ds4key_{factor}) }},\n')
+                for factor in ('RN', 'RP', 'RQ', 'Vi', 'Vf'):
+                    f.write(f'  .{factor} = {{ .s=0, .n=0, .p=nullptr }},\n')
+                f.write('};\n')
+                f.write('\n')
+
+                f.write('/* DS4ID as parts */\n')
+                for page_name, data in ds4id_str.items():
+                    f.write(emit_c_array(data, f'__ds4_{page_name}', 'unsigned char', chunk_size=16))
+                    f.write('\n')
+
+                f.write('#define HAVE_DS4_KEY 1\n')
 
 anybase = functools.partial(int, base=0)
 
@@ -77,13 +155,10 @@ def parse_args():
     p.add_argument('file_contains_ds4key', help='Path to a file that contains the DS4Key.')
     p.add_argument('-o', '--output-prefix', help='Prefix of output file.')
     p.add_argument('-p', '--e-padding-size', type=anybase, default=0xfc, help='Padding size for exponent assuming 32-bit usable exponent (default: 0xfc).')
+    p.add_argument('--gen-passinglink-ds4key', action='store_true', help='Generate PassingLink DS4Key header (experimental).')
     return p, p.parse_args()
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print(f'Usage: {sys.argv[0]} file-contains-ds4key [output-prefix] [e-padding-size]')
-        sys.exit(1)
-
     p, args = parse_args()
     ds4key_len = 0x490 + args.e_padding_size + 4
 
@@ -100,7 +175,7 @@ if __name__ == '__main__':
         print(f'Found identity block @ 0x{key_abs_offset:x}')
         output_prefix_offset = f'{args.output_prefix}_0x{key_abs_offset:x}' if args.output_prefix is not None else None
         try:
-            parse_and_extract_key(fw, key_rel_offset, args.e_padding_size, output_prefix_offset)
+            parse_and_extract_key(fw, key_rel_offset, args.e_padding_size, output_prefix_offset, args.gen_passinglink_ds4key)
         except Exception as e:
             print(f'Unable to extract identity block @ 0x{key_abs_offset:x}: {str(e)}')
             traceback.print_exc()
